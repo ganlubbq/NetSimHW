bc4bd7373ec8cac04b00a58868214fac919777d9
mychele <michele.polese@gmail.com>
2015-05-11 23:55:08 +0200

    Fixed things on GeRaF iterative, still not working

--- HW3/code/GeRaFIterative.m
+++ HW3/code/GeRaFIterative.m
@@ -1,5 +1,5 @@
 %% GeRaF iterative approach
-clear 
+clear
 
 nu = 10; % number of intervals per unit step
 D = 5;
@@ -8,27 +8,32 @@ M = 5; % average number of active nodes in the area
 
 P = zeros(numint, numint);
 
-for i = 1:numint-nu - 1 % up to the states that are outside the area of dst
-    D_st = D - i/nu;
+for i = 1:numint - nu - 1 % up to the states that are outside the area of dst
+    D_st = D - (i-1)/nu;
     % compute the areas
-    area = zeros(1, nu);
-    for j = 1:nu
+    area = zeros(1, nu+1);
+    for j = 0:nu
         r = D_st - j/nu;
         w = (D_st^2 - r^2 + 1)/(2*D_st);
-        area(j) = circle_int_func(w, 1) + circle_int_func(D_st - w, r);
+        area(j+1) = circle_int_func(w, 1) + circle_int_func(D_st - w, r);
     end
     % compute the probabilities
-    for j = 1:nu-1
-        P(i, i - 1 + j) = exp(-M/pi * area(j))*(1-exp(-M/pi * area(j+1)));
+    for j = 1:nu-1 % actually 0:nu-2, but for MATLAB indexing...
+        P(i, i + j - 1) = (1-exp(-M/pi * area(j)))*exp(-M/pi * area(j+1));
     end
+    P(i, i + nu - 1) = 1-exp(-M/pi * area(nu));
 end
 
 for i = numint - nu:numint % absorbing states
     P(i, i) = 1;
 end
 
-v = zeros(1, numint-nu - 1);
-for r = 1:numint-nu-1
-    k = numint-nu - r;
-    v(k) = 0;
+v = zeros(1, numint);
+for r = numint - nu:numint
+    v(r) = 1;
+end
+k = numint - nu - 1;
+while k > 0
+    v(k) = (1+sum(P(k, k:k+1+nu-1).*v(k:k+1+nu-1)))/(1-P(k, k));
+    k = k - 1;
 end
