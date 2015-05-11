fbeddcf8d1f641c325a7109e8f5aed6daa65afc6
mychele <michele.polese@gmail.com>
2015-05-11 22:40:41 +0200

    Fixed a pi factor in GeRaF step

--- HW3/code/GeRaFstep.m
+++ HW3/code/GeRaFstep.m
@@ -2,7 +2,7 @@ function [ D_step ] = GeRaFstep( D, M )
 % Function that simulates a step in GeRaF protocol
 
 % The number of nodes in half of the circle is a Poisson of mean M pi/2
-nnodes = poisson_cdfinv(M*pi/2);
+nnodes = poisson_cdfinv(M/2);
 if (nnodes > 0)
     position = halfCircleUni(nnodes);
     distance = sqrt((D - position(:, 1)).^2 + position(:, 2).^2);
