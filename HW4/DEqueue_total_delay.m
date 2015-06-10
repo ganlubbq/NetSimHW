288376529b9665545838f07a60d259933e240435
mychele <michele.polese@gmail.com>
2015-06-10 19:21:23 +0200

    Minor detail changed

--- HW4/DEqueue_total_delay.m
+++ HW4/DEqueue_total_delay.m
@@ -6,7 +6,7 @@ clc
 %% Ex. 1, case a
 number_of_events = 10^5;
 number_of_desired_renewals = 10^4;
-numsim = 2;
+numsim = 20;
 load('../HW3/code/queueB.mat') % to make it as in the previous homework
 rho_vec_gg = rho_b; %[0.01, 0.1:0.05:0.9, 0.99];
 delay_gg = zeros(length(rho_vec_gg), numsim);
@@ -23,7 +23,7 @@ save('degg1', 'rho_vec_gg', 'delay_gg', 'rho_est_gg')
 %% Ex. 1, case b
 number_of_events = 10^5;
 number_of_desired_renewals = 10^4;
-numsim = 2;
+numsim = 20;
 rho_vec_mg = [0.01, 0.1:0.05:0.9, 0.99];
 delay_mg = zeros(length(rho_vec_mg), numsim);
 rho_est_mg = zeros(length(rho_vec_mg), numsim);
@@ -39,7 +39,7 @@ save('demg1', 'rho_vec_mg', 'delay_mg', 'rho_est_mg')
 %% Ex. 1, case MM1
 number_of_events = 10^5;
 number_of_desired_renewals = 10^4;
-numsim = 2;
+numsim = 20;
 rho_vec_mm = [0.01, 0.1:0.05:0.9, 0.99];
 delay_mm = zeros(length(rho_vec_mm), numsim);
 rho_est_mm = zeros(length(rho_vec_mm), numsim);
